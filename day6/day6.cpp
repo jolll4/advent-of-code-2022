6b17f703be073eaa43c4d6c462e847aed2758132
Joel Arantola <joel.arantola@elekta.com>
2023-05-23 09:21:42 +0300

    clean code practice

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
 #include <fstream>
 
-int searcher(std::string row, int n) {
-    std::string sample = "";
-    for(unsigned int i = 0; i < row.length(); i++) {
-        if (i > n-1) {
-            sample.erase(0,1);
-            sample.push_back(row[i]);
-            bool duplicateFound = false;
-            for(unsigned int j = 0; j < n; j++) {
-                for(unsigned int k = 0; k < n; k++) {
+void appendNewLetter(std::string* sample, char addedLetter) {
+    sample->push_back(addedLetter);
+
+}
+
+void eraseFirstappendNewLetter(std::string* sample, char addedLetter) {
+    sample->erase(0,1);
+    appendNewLetter(sample, addedLetter);
+}
+
+bool checkIfKeysAreUnique(std::string sample, int keySetSize) {
+    bool keysAreUnique = true;
+    for(unsigned int j = 0; j < keySetSize; j++) {
+                for(unsigned int k = 0; k < keySetSize; k++) {
                     if(j != k && sample[j] == sample[k]) {
-                        duplicateFound = true;
+                        keysAreUnique = false;
                     }
                 }
             }
-            if(!duplicateFound) {
-                std::cout << sample << "\n";
-                std::cout << i+1 << "\n";
+    return keysAreUnique;
+}
+
+void printAnswer(std::string sample, int charLocation) {
+    std::cout << "The first matching text is " << sample << std::endl;
+    std::cout << "which ends at char number " << charLocation+1 << std::endl;
+}
+
+void findFirstSetOfUniqueKeys(std::string row, int keySetSize) {
+    std::string sample = "";
+    for(unsigned int i = 0; i < row.length(); i++) {
+        if (i > keySetSize-1) {
+            eraseFirstappendNewLetter(&sample, row[i]);
+            if(checkIfKeysAreUnique(sample, keySetSize)) {
+                printAnswer(sample, i);
                 break;
             }
         } else {
-            sample.push_back(row[i]);
+            appendNewLetter(&sample, row[i]);
         }
     }
-    return 0;
 }
 
-int main() {
+std::string readInputFileContent() {
     std::fstream file;
     std::string row;
 
     file.open("day6-input.txt");
     getline(file, row);
     file.close();
+    return row;
+}
 
-    searcher(row, 4);
-    searcher(row, 14);
+int main() {
+    std::string row = readInputFileContent();
+    findFirstSetOfUniqueKeys(row, 4);
+    findFirstSetOfUniqueKeys(row, 14);
 
     return 0;
 }
