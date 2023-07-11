ab16ed015cdd79bf06199d670fbb44132140301b
Joel Arantola <joel.arantola@elekta.com>
2023-07-11 16:12:01 +0300

    day 8

--- day8/day8.cpp
+++ day8/day8.cpp
@@ -1,64 +1,117 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>    
+#include <vector>
+#include <typeinfo>
 
 using namespace std;
 
-vector<string> readInputFileContent(string fileName) {
+vector<int> convertRowToIntVector(string inputRow) {
+    vector<int> outputRow;
+    for(unsigned int i = 0; i < inputRow.size(); i++) {
+        outputRow.push_back(inputRow[i] - 48);
+    }
+    return outputRow;
+}
+
+vector<vector<int>> readInputFileContent(string fileName) {
     fstream file;
     string rowOfText;
-    vector<string> mapOfTrees;
+    vector<vector<int>> mapOfTrees;
 
     file.open(fileName);
     getline(file, rowOfText);
-    mapOfTrees.push_back(rowOfText);
+    mapOfTrees.push_back(convertRowToIntVector(rowOfText));
     while(getline(file, rowOfText)) {
-        mapOfTrees.push_back(rowOfText);
+        mapOfTrees.push_back(convertRowToIntVector(rowOfText));
     }
     file.close();
     return mapOfTrees;
 }
 
-int countVertical(vector<string> mapOfTrees, unsigned int columnCount) {
-    int verticalSum = 0;
-    for(unsigned int rowNumber = 1; rowNumber < mapOfTrees.size(); rowNumber++) {
-        int highestInColumn = 0;
+vector<vector<int>> countVertical(vector<vector<int>> mapOfTrees, vector<vector<int>> visibleTrees, unsigned int columnCount) {
+    for(unsigned int rowNumber = 0; rowNumber < mapOfTrees.size(); rowNumber++) {
+        int highestInColumn = -1;
         for(unsigned int columnNumber = 0; columnNumber < columnCount; columnNumber++) {
             if(mapOfTrees[rowNumber][columnNumber] > highestInColumn) {
                 highestInColumn = mapOfTrees[rowNumber][columnNumber];
-                verticalSum++;
+                visibleTrees[rowNumber][columnNumber] = 1;
             }
         }
     }
-    return verticalSum;
+    for(unsigned int rowNumber = mapOfTrees.size()-1; rowNumber > 0; rowNumber--) {
+        int highestInColumn = -1;
+        for(unsigned int columnNumber = columnCount-1; columnNumber > 0; columnNumber--) {
+            if(mapOfTrees[rowNumber][columnNumber] > highestInColumn) {
+                highestInColumn = mapOfTrees[rowNumber][columnNumber];
+                visibleTrees[rowNumber][columnNumber] = 1;
+            }
+        }
+    }
+    return visibleTrees;
 }
 
-int countHorizontal(vector<string> mapOfTrees, unsigned int columnCount) {
-    int horizontalSum = 0;
-    for(unsigned int columnNumber = 1; columnNumber < columnCount; columnNumber++) {
-        int highestInRow = 0;
+vector<vector<int>> countHorizontal(vector<vector<int>> mapOfTrees, vector<vector<int>> visibleTrees, unsigned int columnCount) {
+    for(unsigned int columnNumber = 0; columnNumber < columnCount; columnNumber++) {
+        int highestInRow = -1;
         for(unsigned int rowNumber = 0; rowNumber < mapOfTrees.size(); rowNumber++) {
             if(mapOfTrees[rowNumber][columnNumber] > highestInRow) {
                 highestInRow = mapOfTrees[rowNumber][columnNumber];
-                horizontalSum++;
+                visibleTrees[rowNumber][columnNumber] = 1;
             }
         }
     }
-    return horizontalSum;
+    for(unsigned int columnNumber = columnCount-1; columnNumber > 0; columnNumber--) {
+        int highestInRow = -1;
+        for(unsigned int rowNumber = mapOfTrees.size()-1; rowNumber > 0; rowNumber--) {
+            if(mapOfTrees[rowNumber][columnNumber] > highestInRow) {
+                highestInRow = mapOfTrees[rowNumber][columnNumber];
+                visibleTrees[rowNumber][columnNumber] = 1;
+            }
+        }
+    }
+    return visibleTrees;
 }
 
-int countVisibleTrees(vector<string> mapOfTrees) {
+vector<vector<int>> createEmptyForest(int length, int height) {
+    vector<vector<int>> emptyForest;
+    vector<int> emptyRow;
+    for(unsigned int i = 0; i < length; i++) {
+        emptyRow.push_back(0);
+    }
+    for(unsigned int i = 0; i < height; i++) {
+        emptyForest.push_back(emptyRow);
+    }
+    return emptyForest;
+}
+
+int findVisibleTrees(vector<vector<int>> visibleTrees) {
+    int sum = 0;
+    for(unsigned int rowNumber = 0; rowNumber < visibleTrees.size(); rowNumber++) {
+        for(unsigned int columnNumber = 0; columnNumber < visibleTrees[0].size(); columnNumber++) {
+            if(visibleTrees[rowNumber][columnNumber] > 0) {
+                sum++;
+                cout << 1;
+            } else {
+                cout << 0;
+            }
+        }
+        cout << endl;
+    }
+    return sum;
+}
+
+int countVisibleTrees(vector<vector<int>> mapOfTrees) {
     unsigned int columnCount = mapOfTrees[0].size();
-    int verticalSum = countVertical(mapOfTrees, columnCount);
-    int horizontalSum = countHorizontal(mapOfTrees, columnCount);
-    cout << verticalSum << " " << horizontalSum << " " << endl;
-    return verticalSum + horizontalSum + 4;
+    vector<vector<int>> visibleTrees = createEmptyForest(columnCount, mapOfTrees.size());
+    visibleTrees = countVertical(mapOfTrees, visibleTrees, columnCount);
+    visibleTrees = countHorizontal(mapOfTrees, visibleTrees, columnCount);
+    return findVisibleTrees(visibleTrees);
 }
 
 int main() {
-    vector<string> mapOfTrees = readInputFileContent("day8-input.txt");
-    int visibleTrees = countVisibleTrees(mapOfTrees);
-    cout << visibleTrees << endl;
+    vector<vector<int>> mapOfTrees = readInputFileContent("day8-input.txt");
+    int visibleTreesCount = countVisibleTrees(mapOfTrees);
+    cout << visibleTreesCount << endl;
     return 0;
 }
